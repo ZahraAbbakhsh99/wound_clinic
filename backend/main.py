b097487ea8cb67b9a04e19e81f5d8bb5f41eb901
Arezoo <138774289+ZahraAbbakhsh99@users.noreply.github.com>
2025-12-03 10:43:15 +0330

    feat: add test endpoint to verify store static files

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, UploadFile
 from sqlalchemy.orm import Session
 from core.database import get_db, engine
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(title="Wound Clinic Backend")
+
+app.mount("/media", StaticFiles(directory="media"), name="media")
 
 @app.on_event("startup")
 async def on_startup():
@@ -12,6 +15,15 @@ async def on_startup():
 @app.get("/")
 def test_connection(db: Session = Depends(get_db)):
     return {"message": "Database connected successfully!"}
+
+@app.post("/upload")
+async def upload_image(file: UploadFile):
+    file_location = f"media/images/{file.filename}"
+    with open(file_location, "wb") as f:
+        f.write(await file.read())
+
+    url = f"/media/images/{file.filename}"
+    return {"url": url}
 
 @app.on_event("shutdown")
 async def on_shutdown():
