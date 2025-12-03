c459574682ce2bc1938c5511b445fbcd1f6532d4
Arezoo <138774289+ZahraAbbakhsh99@users.noreply.github.com>
2025-12-03 10:34:19 +0330

    feat(models): implement Enums

---
+++ backend/models/enums.py
@@ -0,0 +1,19 @@
+import enum
+
+# Enums
+class ContentStatus(str, enum.Enum):
+    pending = "pending"
+    approved = "approved"
+    rejected = "rejected"
+
+class ActiveStatus(str, enum.Enum):
+    active = "active"
+    inactive = "inactive"
+
+class WoundCategory(str, enum.Enum):
+    diabetic_ulcer = "diabetic_ulcer"
+    pressure_ulcer = "pressure_ulcer"
+    vascular_ulcer = "vascular_ulcer"
+    surgery = "surgery"
+    burns = "burns"
+    general = "general"
